5cfc04cdedd147f47bf9ef92ac616088c1c3a8a8
Nahuel PeFu <daro.nahuel@gmail.com>
2022-04-12 10:15:26 +0200

    feat: use className for css instead of object type target

--- project1/src/components/Navbar.js
+++ project1/src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Navbar(){
     return (
         <nav>
-            <img src={require("../images/reactjs-icon.png")} alt="react-icon"/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={require("../images/reactjs-icon.png")} alt="react-icon" className='nav--icon'/>
+            <h3 className='nav--logo_text'>ReactFacts</h3>
+            <h4 className='nav--title'>React Course - Project 1</h4>
         </nav>
     );
 }
